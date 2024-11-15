d09e60a4c820fd369de3d90067ff14a6ffef22c3
ooumayma <oumaymaelfahsi@gmail.com>
2024-11-15 19:05:09 +0100

    add some modifications

--- handlers/handler.go
+++ handlers/handler.go
@@ -10,12 +10,14 @@ var API = "https://groupietrackers.herokuapp.com/api"
 
 type ArtistHanlder struct{}
 
-var artistUrls, _ = regexp.Compile(`^artists\?id=\d+$`)
+var (
+	Artist        []artist
+	artistUrls, _ = regexp.Compile(`^artists\?id=\d+$`)
+)
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	var artist []artist
-	FetchData(&artist)
-	renderTemplate(w, "index.html", artist)
+	FetchData(&Artist)
+	renderTemplate(w, "index.html", Artist)
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
@@ -27,25 +29,34 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 	)
 
 	user_id, _ := strconv.Atoi(r.URL.Query().Get("id"))
-	api := API + "/artists/" + strconv.Itoa(user_id)
-	FetchData(&artist, api)
-	_, ok := artist.Locations.(string)
-	if ok {
-		FetchData(&location, artist.Locations.(string))
-		FetchData(&relation, artist.Relations.(string))
-		FetchData(&dates, artist.ConcertDates.(string))
-		artist.ConcertDates = dates
-		artist.Locations = location
-		artist.Relations = relation
+	if Artist != nil {
+		_, ok := Artist[user_id-1].Locations.(string)
+		if ok {
+			FetchData(&Artist[user_id-1].Locations, Artist[user_id-1].Locations.(string))
+			FetchData(&Artist[user_id-1].ConcertDates, Artist[user_id-1].ConcertDates.(string))
+			FetchData(&Artist[user_id-1].Relations, Artist[user_id-1].Relations.(string))
+		}
+		renderTemplate(w, "artist.html", Artist)
+
+	} else {
+		api := API + "/artists/" + strconv.Itoa(user_id)
+		FetchData(&artist, api)
+		_, ok := artist.Locations.(string)
+		if ok {
+			FetchData(&location, artist.Locations.(string))
+			FetchData(&relation, artist.Relations.(string))
+			FetchData(&dates, artist.ConcertDates.(string))
+			artist.ConcertDates = dates
+			artist.Locations = location
+			artist.Relations = relation
+		}
+		renderTemplate(w, "artist.html", artist)
 	}
-	renderTemplate(w, "artist.html", artist)
 }
 
-
-
-
-
-
+// There is a problem in this demarch
+// while we can use the function to fetch the data
+// But the the first way is more useful
 
 func NotFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
