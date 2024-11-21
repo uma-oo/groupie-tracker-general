4b241bf35a05a42e5de06ea51c0f75ea6d551b49
Oumayma El-fahsi <oumayma.elfahsi99@gmail.com>
2024-11-21 17:10:37 +0100

    add new version

--- main.go
+++ main.go
@@ -2,28 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"text/template"
 
 	"groupie/handlers"
 )
 
-var Templates *template.Template
+// var Templates *template.Template
 
-func init(){
-  var err error
-   Templates, err = template.ParseGlob("./templates/*.html")
-   if err!= nil {
-      log.Fatal("The files that need to be")
-   }
-}
-
-
-
+// func init(){
+//   var err error
+//    Templates, err = template.ParseGlob("./templates/*.html")
+//    if err!= nil {
+//       log.Fatal("The files that need to be")
+//    }
+// }
 
 func main() {
 	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("./assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets", fs))
 	mux.Handle("/", &handlers.ArtistHanlder{})
 	fmt.Println("Listening on http://localhost:3535")
 	http.ListenAndServe(":3535", mux)
